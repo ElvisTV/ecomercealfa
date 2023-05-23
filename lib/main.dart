ca7276ce2373342f9e00cbc0c2decfef31a94e84
elvis arturo tezen villanueva <elvistezen@outlook.com>
2023-05-22 19:28:23 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:vitmaecomerce/routes.dart';
+import 'package:vitmaecomerce/screens/profile/profile_screen.dart';
+import 'package:vitmaecomerce/screens/splash/splash_screen.dart';
+import 'package:vitmaecomerce/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: theme(),
+      // home: SplashScreen(),
+      // We use routeName so that we dont need to remember the name
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
